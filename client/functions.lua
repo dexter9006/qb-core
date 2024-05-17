0ae40a04ad646156d551c72676cbc728b5476196
dexter9006 <52006237+dexter9006@users.noreply.github.com>
2024-05-17 18:52:01 +0100

    experimental functions.lua

--- client/functions.lua
+++ client/functions.lua
@@ -58,30 +58,29 @@ end
 ---@return number - The time at which the entity was looked at
 function QBCore.Functions.LookAtEntity(entity, timeout, speed)
     local involved = GetInvokingResource()
+    local turnPromise = promise.new()
     if not DoesEntityExist(entity) then
-        turnPromise:reject(involved .. ' :^1  Entity does not exist')
+        turnPromise:reject(involved .. ' :^1 Entity does not exist')
         return turnPromise.value
     end
-    if not type(entity) == 'number' then
-        turnPromise:reject(involved .. ' :^1  Entity must be a number')
+    if type(entity) ~= 'number' then
+        turnPromise:reject(involved .. ' :^1 Entity must be a number')
         return turnPromise.value
     end
-    if not type(speed) == 'number' then
-        turnPromise:reject(involved .. ' :^1  Speed must be a number')
+    if type(speed) ~= 'number' then
+        turnPromise:reject(involved .. ' :^1 Speed must be a number')
         return turnPromise.value
     end
-    if speed > 5.0 then speed = 5.0 end
-    if timeout > 5000 then timeout = 5000 end
+    speed = math.min(speed, 5.0)
+    timeout = math.min(timeout, 5000)
 
     local ped = PlayerPedId()
     local playerPos = GetEntityCoords(ped)
-
     local targetPos = GetEntityCoords(entity)
     local dx = targetPos.x - playerPos.x
     local dy = targetPos.y - playerPos.y
     local targetHeading = GetHeadingFromVector_2d(dx, dy)
 
-    local turnSpeed = speed
     local startTimeout = GetGameTimer()
     while true do
         local currentHeading = GetEntityHeading(ped)
@@ -89,14 +88,12 @@ function QBCore.Functions.LookAtEntity(entity, timeout, speed)
         if math.abs(diff) < 2 then
             break
         end
-
         if diff < -180 then
             diff = diff + 360
         elseif diff > 180 then
             diff = diff - 360
         end
-
-        turnSpeed = speed + (2.5 - speed) * (1 - math.abs(diff) / 180)
+        local turnSpeed = speed + (2.5 - speed) * (1 - math.abs(diff) / 180)
 
         if diff > 0 then
             currentHeading = currentHeading + turnSpeed
@@ -108,6 +105,7 @@ function QBCore.Functions.LookAtEntity(entity, timeout, speed)
         if (startTimeout + timeout) < GetGameTimer() then break end
     end
     SetEntityHeading(ped, targetHeading)
+    return turnPromise
 end
 
 -- Function to run an animation
