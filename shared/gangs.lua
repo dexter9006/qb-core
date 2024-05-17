b02480d972824144c2643e24603278f1519ee513
dexter9006 <52006237+dexter9006@users.noreply.github.com>
2024-05-17 19:16:25 +0100

    Update gangs.lua

--- shared/gangs.lua
+++ shared/gangs.lua
@@ -1,57 +1,57 @@
 QBShared = QBShared or {}
 QBShared.Gangs = {
-    none = { label = 'No Gang', grades = { ['0'] = { name = 'Unaffiliated' } } },
+    none = { label = 'No Gang', grades = { ['0'] = { name = 'Aucune affiliation' } } },
     lostmc = {
         label = 'The Lost MC',
         grades = {
-            ['0'] = { name = 'Recruit' },
-            ['1'] = { name = 'Enforcer' },
-            ['2'] = { name = 'Shot Caller' },
+            ['0'] = { name = 'Recrue' },
+            ['1'] = { name = 'Soldat' },
+            ['2'] = { name = 'Bras Droit' },
             ['3'] = { name = 'Boss', isboss = true },
         },
     },
     ballas = {
         label = 'Ballas',
         grades = {
-            ['0'] = { name = 'Recruit' },
-            ['1'] = { name = 'Enforcer' },
-            ['2'] = { name = 'Shot Caller' },
-            ['3'] = { name = 'Boss', isboss = true },
-        },
-    },
-    vagos = {
-        label = 'Vagos',
-        grades = {
-            ['0'] = { name = 'Recruit' },
-            ['1'] = { name = 'Enforcer' },
-            ['2'] = { name = 'Shot Caller' },
-            ['3'] = { name = 'Boss', isboss = true },
-        },
-    },
-    cartel = {
-        label = 'Cartel',
-        grades = {
-            ['0'] = { name = 'Recruit' },
-            ['1'] = { name = 'Enforcer' },
-            ['2'] = { name = 'Shot Caller' },
+            ['0'] = { name = 'Recrue' },
+            ['1'] = { name = 'Soldat' },
+            ['2'] = { name = 'Bras Droit' },
             ['3'] = { name = 'Boss', isboss = true },
         },
     },
     families = {
         label = 'Families',
         grades = {
-            ['0'] = { name = 'Recruit' },
-            ['1'] = { name = 'Enforcer' },
-            ['2'] = { name = 'Shot Caller' },
+            ['0'] = { name = 'Recrue' },
+            ['1'] = { name = 'Soldat' },
+            ['2'] = { name = 'Bras Droit' },
+            ['3'] = { name = 'Boss', isboss = true },
+        },
+    },
+    vagos = {
+        label = 'Vagos',
+        grades = {
+            ['0'] = { name = 'Recrue' },
+            ['1'] = { name = 'Soldat' },
+            ['2'] = { name = 'Bras Droit' },
+            ['3'] = { name = 'Boss', isboss = true },
+        },
+    },
+    cartel = {
+        label = 'Cartel',
+        grades = {
+            ['0'] = { name = 'Recrue' },
+            ['1'] = { name = 'Soldat' },
+            ['2'] = { name = 'Bras Droit' },
             ['3'] = { name = 'Boss', isboss = true },
         },
     },
     triads = {
         label = 'Triads',
         grades = {
-            ['0'] = { name = 'Recruit' },
-            ['1'] = { name = 'Enforcer' },
-            ['2'] = { name = 'Shot Caller' },
+            ['0'] = { name = 'Recrue' },
+            ['1'] = { name = 'Soldat' },
+            ['2'] = { name = 'Bras Droit' },
             ['3'] = { name = 'Boss', isboss = true },
         },
     }
